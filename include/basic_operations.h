db510a09e539267099283d6b7afe8aea7abc3e2c
Guerchom Kubaluka <guerchomkubaluka@gmail.com>
2026-08-23 20:41:42 +0100

    feat( basic operations) : implementation de la division et la multiplication

--- include/basic_operations.h
+++ include/basic_operations.h
@@ -1,1 +1,30 @@
 //implementer le prototype de chaque fonction
+#ifndef BASIC_OPERATIONS_H
+#define BASIC_OPERATIONS_H
+
+/*
+ * Additionne deux nombres.
+ */
+double additionner(double a, double b);
+
+/*
+ * Soustrait le deuxième nombre du premier.
+ */
+double soustraire(double a, double b);
+
+/*
+ * Multiplie deux nombres.
+ */
+double multiplier(double a, double b);
+
+/*
+ * Divise a par b.
+ *
+ * Retourne 1 si la division est réussie.
+ * Retourne 0 si b est égal à zéro.
+ *
+ * Le résultat est stocké dans resultat.
+ */
+int diviser(double a, double b, double *resultat);
+
+#endif
--- src/basic_operations.c
+++ src/basic_operations.c
@@ -1,4 +1,5 @@
-//guerschom  & benel & isaac: module: basic operations
+#include "../include/basic_operations.h"
+//guerchom  & benel & isaac: module: basic operations
 //implementer les fonctions ici
 
 //addition: prend en parametre deux nombre et les additionne
@@ -6,8 +7,30 @@
 //soustraction: prend en parametre deux nombre et les soustrait
 
 //multipication: prend en parametre deux nombre et les multiplie
+double multiplier(double a, double b){
+    return a*b;
+}
 
 // division : prend en parametre deux nombre et les divise en faisant attention à ce que le deuxieme nombre sois different de zero, si c'est le cas lever une exception
+/*
+ * Divise deux nombres.
+ *
+ * Retourne 1 si la division est possible.
+ * Retourne 0 si le diviseur est nul.
+ *
+ * Le résultat est stocké dans resultat.
+ */
+int diviser(double a, double b, double *resultat)
+{
+    if (b == 0)
+    {
+        return 0;
+    }
+
+    *resultat = a / b;
+
+    return 1;
+}
 
 //test des chaque fonction individuellement en ligne de commande
 
